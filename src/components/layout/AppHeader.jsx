3e5cd823fe2fa878b67ae77de221080fd8d37180
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-04-09 23:03:03 +0300

    Header. стилизация | добавлен Select, Button

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,17 +1,66 @@
-import { Layout } from "antd";
+import { Button, Layout, Select, Space } from "antd";
 
 const headerStyle = {
-  textAlign:'center',
-  color:'#fff',
-  height:60,
-  paddingInline:48,
-  lineHeight:'64px',
-  backgroundColor:'#4096ff',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: '1rem',
+  width: '100%',
+  textAlign: 'center',
+  height: 60,
 };
 
-export default function AppHeader() {
-  return (
-    <Layout.Header style = {headerStyle}>Header</Layout.Header>
+const options = [
+  {
+    label: 'China',
+    value: 'china',
+    emoji: '🇨🇳',
+    desc: 'China (中国)',
+  },
+  {
+    label: 'USA',
+    value: 'usa',
+    emoji: '🇺🇸',
+    desc: 'USA (美国)',
+  },
+  {
+    label: 'Japan',
+    value: 'japan',
+    emoji: '🇯🇵',
+    desc: 'Japan (日本)',
+  },
+  {
+    label: 'Korea',
+    value: 'korea',
+    emoji: '🇰🇷',
+    desc: 'Korea (韩国)',
+  },
+];
 
+
+export default function AppHeader () {
+  return (
+    <Layout.Header style = {headerStyle}>
+      <Select
+        style = {{width: 250}}
+        placeholder = "select one country"
+        defaultValue = {['china']}
+        // onChange = {handleChange}
+        options = {options}
+        optionRender = {(option) => (
+          <Space>
+            <span
+              role = "img"
+              aria-label = {option.data.label}
+            >
+              {option.data.emoji}
+            </span>
+            {option.data.desc}
+          </Space>
+        )}
+      />
+      <Button type = "primary">Add asset</Button>
+
+    </Layout.Header>
   )
 }
