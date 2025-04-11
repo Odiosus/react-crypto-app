05d42a95a6611352bc982f4a36d34de30e2f6756
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-04-11 17:18:35 +0300

    Header. Modal > CoinInfoModal добавлен Typography.Paragraph с изменениями курса по часам, дням, неделям + стилизация

--- src/components/CoinInfoModal.jsx
+++ src/components/CoinInfoModal.jsx
@@ -1,25 +1,62 @@
-import { Flex, Tag, Typography } from "antd";
+import { Divider, Flex, Tag, Typography } from "antd";
 
 // выводим инфо о коине. принимает параметр "coin"
 export default function CoinInfoModal ({coin}) {
   return (
-    <Flex
-      // children={}
-      align='center'
-    >
-      <img
-        src={coin.icon}
-        alt={coin.name}
-        width={40}/>
-      <Typography.Title
-        level={2}
-        style={{
-          margin: 0,
-          paddingLeft: 10
-        }}
+    <>
+      <Flex
+        align='center'
       >
-        ({coin.symbol}) {coin.name}
-      </Typography.Title>
-    </Flex>
+        <img
+          src={coin.icon}
+          alt={coin.name}
+          width={40}/>
+        <Typography.Title
+          level={2}
+          style={{
+            margin: 0,
+            paddingLeft: 10
+          }}
+        >
+          ({coin.symbol}) {coin.name}
+        </Typography.Title>
+      </Flex>
+      <Divider/>
+      <Typography.Paragraph>
+        <Typography.Text strong>
+          1 hour:
+        </Typography.Text>
+        <Tag
+          color={coin.priceChange1h > 0
+            ? 'green'
+            : 'red'}
+          style={{marginLeft: 5}}
+        >
+          {coin.priceChange1h}%
+        </Tag>
+        <Typography.Text strong>
+          1 day:
+        </Typography.Text>
+        <Tag
+          color={coin.priceChange1d > 0
+            ? 'green'
+            : 'red'}
+          style={{marginLeft: 5}}
+        >
+          {coin.priceChange1d}%
+        </Tag>
+        <Typography.Text strong>
+          1 week:
+        </Typography.Text>
+        <Tag
+          color={coin.priceChange1w > 0
+            ? 'green'
+            : 'red'}
+          style={{marginLeft: 5}}
+        >
+          {coin.priceChange1w}%
+        </Tag>
+      </Typography.Paragraph>
+    </>
   )
 }
