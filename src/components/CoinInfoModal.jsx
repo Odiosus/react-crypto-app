f6fb6e02389adbb70dbd5d860f623c2b01260aaf
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-04-11 16:59:52 +0300

    Header. Modal > CoinInfoModal модернизирован компонент + стилизация

--- src/components/CoinInfoModal.jsx
+++ src/components/CoinInfoModal.jsx
@@ -1,6 +1,25 @@
-// выводим имя коина в заголовке h2. принимает параметр "coin"
+import { Flex, Tag, Typography } from "antd";
+
+// выводим инфо о коине. принимает параметр "coin"
 export default function CoinInfoModal ({coin}) {
   return (
-    <h2>{coin.name}</h2>
+    <Flex
+      // children={}
+      align='center'
+    >
+      <img
+        src={coin.icon}
+        alt={coin.name}
+        width={40}/>
+      <Typography.Title
+        level={2}
+        style={{
+          margin: 0,
+          paddingLeft: 10
+        }}
+      >
+        ({coin.symbol}) {coin.name}
+      </Typography.Title>
+    </Flex>
   )
 }
