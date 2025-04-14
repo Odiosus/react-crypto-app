e56f3f99935d64bca0061851c3d69e7e8aa6e41c
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-04-14 23:13:34 +0300

    Header. Modal > Drawer > AddAssetForm > рыба формы

--- src/components/AddAssetForm.jsx
+++ src/components/AddAssetForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Divider, Flex, Select, Space, Typography } from "antd";
+import { Divider, Flex, Form, Input, Select, Space, Typography } from "antd";
 import { useCrypto } from "../context/crypto-context.jsx";
 
 export default function AddAssetForm () {
@@ -53,6 +53,46 @@ export default function AddAssetForm () {
         </Typography.Title>
       </Flex>
       <Divider/>
+      <Form
+        name="basic"
+        labelCol={{span: 8}}
+        wrapperCol={{span: 16}}
+        style={{maxWidth: 600}}
+        initialValues={{remember: true}}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{required: true, message: 'Please input your username!'}]}
+        >
+          <Input/>
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{required: true, message: 'Please input your password!'}]}
+        >
+          <Input.Password/>
+        </Form.Item>
+
+        <Form.Item name="remember"
+          valuePropName="checked"
+          label={null}>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item label={null}>
+          <Button type="primary"
+            htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      );
     </form>
   )
 }
